418c4bed8d8694ee1da974e7f9cd6b724caeb1d8
Anshul <admin@syncspire.com>
2019-06-18 18:21:57 +0530

    Add env variables in a file

--- starterbot.py
+++ starterbot.py
@@ -2,6 +2,15 @@ import os
 import time
 import re
 import slack
+from dotenv import load_dotenv
+load_dotenv()
 
-slack_client = slack.WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
+slack_client = slack.WebClient(token=os.getenv("SLACK_BOT_TOKEN"))
+
+response = slack_client.chat_postMessage(
+        channel='#random',
+        text="Hello world!")
+assert response["ok"]
+assert response["message"]["text"] == "Hello world!"
+    
 print(slack_client)
